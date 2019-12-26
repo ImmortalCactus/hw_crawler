f629524cb3007d3d3db4bf66e9d2878e75affdec
黃竑鈞 <jim@huanghongjunde-MacBook-Pro.local>
2019-12-26 09:39:26 +0800

    update args.py requirements.txt

--- args.py
+++ args.py
@@ -8,7 +8,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--start-date', required = True, help = 'Crawl Start Date')
     parser.add_argument('--end-date',required = True, help = 'Crawl End Date')
-    parser.add_argument('--output.csv')
+    parser.add_argument('--output', default = 'output.csv', help = 'output file name')
     args = parser.parse_args()
     args.start_date = datetime.strptime(args.start_date, '%Y-%m-%d')
     args.end_date = datetime.strptime(args.end_date, '%Y-%m-%d')
